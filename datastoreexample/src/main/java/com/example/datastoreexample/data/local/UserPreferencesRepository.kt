d7e3b5ada1ecfe22c7a9737864fc803111d6a2bc
Kyawkk <kzin5717@gmail.com>
2023-09-17 19:02:24 +0630

    Added data store to datastore example module

---
+++ datastoreexample/src/main/java/com/example/datastoreexample/data/local/UserPreferencesRepository.kt
@@ -0,0 +1,36 @@
+package com.example.datastoreexample.data.local
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class UserPreferencesRepository(
+    private val dataStore: DataStore<Preferences>
+) {
+    val isLinearLayout: Flow<Boolean> =
+        dataStore.data
+            .catch {
+                if (it is IOException){
+                    emit(emptyPreferences())
+                }else throw it
+            }
+            .map { it[IS_LINEAR_LAYOUT] ?: true }
+
+    private companion object {
+        val IS_LINEAR_LAYOUT = booleanPreferencesKey("is_linear_layout")
+    }
+
+    suspend fun saveLayoutPreference(isLinearLayout: Boolean) {
+        dataStore.edit {
+            it[IS_LINEAR_LAYOUT] = isLinearLayout
+        }
+    }
+
+}
--- datastoreexample/src/main/java/com/example/datastoreexample/ui/DesertReleaseViewModel.kt
+++ datastoreexample/src/main/java/com/example/datastoreexample/ui/DesertReleaseViewModel.kt
@@ -2,31 +2,51 @@ package com.example.datastoreexample.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.datastoreexample.DesertReleaseApplication
 import com.example.datastoreexample.R
+import com.example.datastoreexample.data.local.UserPreferencesRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class DesertReleaseViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(DesertReleaseUiState())
+class DesertReleaseViewModel(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
 
-    val uiState : StateFlow<DesertReleaseUiState> = _uiState.asStateFlow()
+    val uiState: StateFlow<DesertReleaseUiState> = userPreferencesRepository.isLinearLayout.map {
+        DesertReleaseUiState(it)
+    }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = DesertReleaseUiState()
+        )
 
-    fun selectLayout (isLinearLayout: Boolean) {
-        _uiState.value = DesertReleaseUiState(isLinearLayout = isLinearLayout)
+    fun selectLayout(isLinearLayout: Boolean) {
+        viewModelScope.launch {
+            userPreferencesRepository.saveLayoutPreference(isLinearLayout)
+        }
     }
 
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
-            initializer { DesertReleaseViewModel() }
+            initializer {
+                val application = (this[APPLICATION_KEY] as DesertReleaseApplication)
+                DesertReleaseViewModel(application.userPreferencesRepository)
+            }
         }
     }
 }
 
 data class DesertReleaseUiState(
-    val isLinearLayout : Boolean = true,
-    val toggleContentDescription : Int = if(isLinearLayout) R.string.grid_layout_toggle else R.string.linear_layout_toggle,
-    val toggleIcon : Int = if (isLinearLayout) R.drawable.ic_grid_layout else R.drawable.ic_linear_layout
+    val isLinearLayout: Boolean = true,
+    val toggleContentDescription: Int = if (isLinearLayout) R.string.grid_layout_toggle else R.string.linear_layout_toggle,
+    val toggleIcon: Int = if (isLinearLayout) R.drawable.ic_grid_layout else R.drawable.ic_linear_layout
 )
